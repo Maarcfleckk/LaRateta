1e8cba45f115e4dfb2a4e874d5a894a6b2b8f398
Fleki <marc.fleck@mataro.epiaedu.cat>
2023-12-14 22:18:04 +0100

    update

---
+++ custom.js
@@ -0,0 +1,37 @@
+// Función para abrir el modal
+function openModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+
+// Cierra el modal si se hace clic fuera de él
+window.onclick = function (event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+};
+
+// Variables para el audio y el estado de reproducción
+var audio = new Audio('./path/to/your/audio.mp3');
+var isPlaying = false;
+
+// Función para pausar o continuar la reproducción del audio
+function toggleAudio() {
+    audio.pause();
+}
+
+// Función para detener la reproducción del audio
+function stopAudio() {
+    audio.pause();
+    audio.currentTime = 0;
+    document.getElementById('playButton').innerText = 'Play';
+    isPlaying = false;
+}
+
+// Resto del código...
--- levels/level-1/script.js
+++ levels/level-1/script.js
@@ -4,25 +4,24 @@ const context = mainCanvas.getContext("2d");
 let initialX;
 let initialY;
 
-const dibujar = (cursorX, cursorY) =>{
-    context.beginPath();
-    context.moveTo(initialX, initialY);
-    context.lineWidth = 8000;
-    context.strokeStyle = "";
-    context.lineCap = "square";
-    context.lineJoin = "round";
-    context.lineTo(cursorX, cursorY);
-    context.stroke();
+const dibujar = (cursorX, cursorY) => {
+  context.beginPath();
+  context.moveTo(initialX, initialY);
+  context.lineWidth = 8000;
+  context.strokeStyle = "";
+  context.lineCap = "square";
+  context.lineJoin = "round";
+  context.lineTo(cursorX, cursorY);
+  context.stroke();
 
-    initialX = cursorX;
-    initialY = cursorY;
-}
-
-const mouseClick = (evt) => {
-    initialX = evt.offsetX;
-    initialY = evt.offsetY;
-    dibujar(initialX, initialY);
+  initialX = cursorX;
+  initialY = cursorY;
 };
 
-mainCanvas.addEventListener('mousedown', mouseClick);
+const mouseClick = (evt) => {
+  initialX = evt.offsetX;
+  initialY = evt.offsetY;
+  dibujar(initialX, initialY);
+};
 
+mainCanvas.addEventListener("mousedown", mouseClick);
