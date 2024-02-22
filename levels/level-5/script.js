3ff533efb7ca58d84a6156c21ce9d1138f4b3f78
Fleki <marc.fleck@mataro.epiaedu.cat>
2024-02-22 22:20:43 +0100

    nivel 5

--- levels/level-5/script.js
+++ levels/level-5/script.js
@@ -1,27 +1,54 @@
-const mainCanvas = document.getElementById("main-canvas");
-const context = mainCanvas.getContext("2d");
-
-let initialX;
-let initialY;
-
-const dibujar = (cursorX, cursorY) => {
-  context.beginPath();
-  context.moveTo(initialX, initialY);
-  context.lineWidth = 8000;
-  context.strokeStyle = "";
-  context.lineCap = "square";
-  context.lineJoin = "round";
-  context.lineTo(cursorX, cursorY);
-  context.stroke();
-
-  initialX = cursorX;
-  initialY = cursorY;
-};
-
-const mouseClick = (evt) => {
-  initialX = evt.offsetX;
-  initialY = evt.offsetY;
-  dibujar(initialX, initialY);
-};
-
-mainCanvas.addEventListener("mousedown", mouseClick);
+function showWinMessage() {
+  const winMessage = document.getElementById("win-message");
+  winMessage.style.display = "block";
+}
+function openErrorMessage() {
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.style.display = "block";
+}
+function closeErrorMessage() {
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.style.display = "none";
+}
+function refreshPage() {
+  location.reload();
+}
+function randomImg() {
+  const imgNumber = Math.floor(Math.random() * 3);
+  if (imgNumber === 0) {
+    return "../../images/level5/lazo.png";
+  } else if (imgNumber === 1) {
+    return "../../images/level5/moneda.png";
+  }
+  return "../../images/level5/escoba.png";
+}
+function handleIncrement() {
+  const resultInput = document.querySelector(".resultInput");
+  return (resultInput.value = parseInt(resultInput.value) + 1);
+}
+function handleDecrement() {
+  const resultInput = document.querySelector(".resultInput");
+  return (resultInput.value = parseInt(resultInput.value) - 1);
+}
+function itemGenerator() {
+  const itemContainer = document.querySelector(".gameItemContainer");
+  let itemNumber = Math.floor(Math.random() * 10) + 1;
+  const imageSrc = randomImg();
+  for (let index = 0; index < itemNumber; index++) {
+    const item = document.createElement("img");
+    item.classList.add("itemImg");
+    item.src = imageSrc;
+    itemContainer.appendChild(item);
+  }
+}
+function checkResult() {
+  const resultInput = document.querySelector(".resultInput");
+  const givenResult = resultInput.value;
+  const result = document.querySelectorAll(".itemImg").length;
+  if (givenResult === result.toString()) {
+    showWinMessage();
+  } else {
+    openErrorMessage();
+  }
+}
+itemGenerator();
